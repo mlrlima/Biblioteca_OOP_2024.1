04ab68a7238301fed95b31be2a30f12bb4ca3c9c
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2024-04-16 13:26:58 -0300

    Update Pessoa.java

--- Projeto1/Pessoa.java
+++ Projeto1/Pessoa.java
@@ -1,7 +1,25 @@
-class Pessoa{
 
-  private String nome, endereco, teelfone;
+public class Pessoa{
+    protected String nome, endereco, telefone;
 
-  
-  
+    void setNome(Stirng nome){
+        this.nome=nome;
+    }
+    String getNome(){
+        return nome;
+    }
+
+    void setEndereco(String endereco){
+        this.endereco=endereco;
+    }
+    String getEndereco(){
+        return endereco;
+    }
+
+    void setTelefone(String telefone){
+        this.telefone=telefone;
+    }
+    String getTelefone(){
+        return telefone;
+    }
 }
