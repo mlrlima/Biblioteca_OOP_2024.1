5c86ec8fe2eaefe1cb18db04a5f1da2c9e8c9769
Roberto Regis <125362362+Roberto20deluxe@users.noreply.github.com>
2024-04-18 15:44:01 -0300

    Cliente.java

---
+++ Projeto1/Cliente.java
@@ -0,0 +1,31 @@
+public class Cliente extends Pessoa  {
+    private int idCliente;
+    private ArrayList<Livro> livrosEmprestados;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public ArrayList<Livro> getLivrosEmprestados() {
+        return livrosEmprestados;
+    }
+
+    public void setLivrosEmprestados(ArrayList<Livro> livrosEmprestados) {
+        this.livrosEmprestados = livrosEmprestados;
+    }
+
+    public void emprestarLivro(Livro livro) {
+        livrosEmprestados.add(livro);
+    }
+
+    public void devolverLivro(Livro livro) {
+        livrosEmprestados.remove(livro);
+    }
+}    
+                
+    
+            
